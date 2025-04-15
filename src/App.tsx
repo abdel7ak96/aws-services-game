4cc07d196511f7bbe0710665d3ca627f7002b30f
Abdelhak Rahmouni <rahmouni.abdelhak.1@gmail.com>
2025-04-15 18:45:00 +0100

    feat: implement initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import Logo from "./assets/logo.png";
+import services from "./assets/data/services.json";
+
 function App() {
-	return <h1 className="text-fuchsia-400 font-bold text-3xl">Hello, Vite World!</h1>;
+	return (
+		<div className="p-10">
+			<div className="flex items-center gap-x-4">
+				<img className="w-20" src={Logo} alt="Logo" />
+				<h1 className="text-4xl font-bold">AWS Cloud Services Game</h1>
+			</div>
+
+			<p className="mt-4 text-lg">
+				Welcome to the AWS Cloud Services Game! Test your knowledge of AWS
+				services and learn more about cloud computing.
+			</p>
+
+			<div className="flex justify-center">
+				<button
+					className="mt-6 px-4 py-2 bg-blue-500 hover:bg-blue-600 hover:shadow-md cursor-pointer text-white rounded-xl"
+					type="button"
+				>
+					Start Game
+				</button>
+			</div>
+
+			<div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 mt-10">
+				{services.map((service) => (
+					<div
+						key={service.category}
+						className="border border-gray-200 p-4 rounded-lg shadow-md"
+					>
+						<h5 className="text-2xl">{service.category}</h5>
+						{service.services.map((s) => (
+							<p key={s.id}>{s.name}</p>
+						))}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
